6d98a6513444a9d94f64a19af98b37dd1bdeb912
Keith Hamm <hkhamm@gmail.com>
2021-10-21 17:40:05 -0700

    updates start]

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-xset s noblank
-xset s off
-xset -dpms
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-
-/usr/bin/chromium-browser --no-sandbox --incognito --start-fullscreen http://localhost
+/usr/bin/chromium-browser --disable-restore-session-state --start-fullscreen http://localhost &
