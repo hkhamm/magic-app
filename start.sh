f784d6314afa131224477f20695f3a817d9f8cfa
Keith Hamm <hkhamm@gmail.com>
2021-10-21 17:10:42 -0700

    udpates start script

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ xset -dpms
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --incognito --start-fullscreen http://localhost
+/usr/bin/chromium-browser --incognito --start-fullscreen http://localhost &
